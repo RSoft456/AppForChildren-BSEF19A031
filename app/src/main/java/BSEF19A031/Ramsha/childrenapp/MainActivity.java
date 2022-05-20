01e2d2f93b05e1bfa769027dc97de1f73cdddd91
DESKTOP-G6FQLNH\user <ramshamehmood456@gmail.com>
2022-05-20 20:52:21 +0500

    Activity 1 added and layout setted

--- app/src/main/java/BSEF19A031/Ramsha/childrenapp/MainActivity.java
+++ app/src/main/java/BSEF19A031/Ramsha/childrenapp/MainActivity.java
@@ -3,12 +3,30 @@ package BSEF19A031.Ramsha.childrenapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Launch,Repo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Launch = findViewById(R.id.LaunchApp);
+        Repo = findViewById(R.id.Repository);
+        Launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Repo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
